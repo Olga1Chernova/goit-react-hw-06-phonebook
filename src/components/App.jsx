8ebdecfedb55fec5c4a38b446ae243ab30aeb977
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-03-06 13:41:52 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+//import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import ContactForm from './ContactForm/ContactForm';
